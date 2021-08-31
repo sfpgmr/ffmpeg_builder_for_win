c3a650c8451a3c970b3ff31002e45ae3a143c274
sfpgmr <sfpgmr@dummy.com>
2021-08-31 16:35:11 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ffmpeg_builder_fpr_win
-ShiftMediaProjectの素晴らしいツール群を使用して、ffmpegをVisual Studio Build Toolsを使ってビルドするPowerShellスクリプトを作ります。
+# ffmpeg_builder_for_win
+
+ShiftMediaProjectの素晴らしいツール群を使用して、ffmpegをVisual Studio Build Toolsを使ってビルドするPowerShellスクリプトです。
+
+ビルド・フローに関しては[FFmpeg/readme\.txt](https://github.com/ShiftMediaProject/FFmpeg/blob/master/SMP/readme.txt)の手順を参考に、YASMを追加インストールするステップを追加しています。
+
+## 実行にあたっての前提条件
+
+* Visual Studio Build Tools がインストールされていること
+* Git For Windowsがインストールされ、コマンドラインで使用できること
+* PowerShellの最新バージョンがインストールされていること
+## 使い方
+
+1. レポジトリをクローンします
+2. x64 Native Tool Command Promptを起動し、クロ－ンしたディレクトリに移動します。
+3. pwshを起動します。
+4. build.ps1を実行します。<br>
+-Cleanを指定すると作業ディレクトリをすべて削除したのち、ビルドします。<br>
+-BuiltTypebuild/rebuild/cleanのオプションが選択できます（規定値：build）。<br>
+-ObjTypeでDebug/Release/DebugDLL/ReleaseDLL/ReleaseDLLStaticDepsのオプションが選択できます。（規定値：Release）<br>
+-CpuTypeでx86/x64が選択できます。（規定値：x64）
+5. msvc\binにオブジェクトが生成されます。ビルドされるのはRelease/Debugのx64版のみです。
+
+## 注意事項
+
+* 途中2回、NASM/YASMのインストールで管理者モードへの切り替え確認ダイアログが出ますので、すべて「はい」としてください。
--- build.ps1
+++ build.ps1
@@ -3,106 +3,138 @@
     Visual Studio Build Toolsを使ってビルドするバッチ
 #>
 
-if(!(Test-Path '.\msvc')){
-  New-Item '.\msvc' -ItemType Directory
-}
 
-if(!(Test-Path '.\msvc\include')){
-  New-Item '.\msvc\include' -ItemType Directory
-}
+param (
+  [switch]$Clean = $false,
+  [ValidateSet("build" , "rebuild", "clean")]$BuildType = "build",
+  [ValidateSet("Debug" , "Release", "DebugDLL","ReleaseDLL","ReleaseDLLStaticDeps")]$ObjType = "Release",
+  [ValidateSet("x86" , "x64")]$CpuType = "x64"
+)
 
-if(!(Test-Path '.\msvc\include\gl')){
-  New-Item '.\msvc\include\gl' -ItemType Directory
-}
-
-if(!(Test-Path '.\msvc\include\KHR')){
-  New-Item '.\msvc\include\KHR' -ItemType Directory
-}
+$ErrorActionPreference = "Stop"
+$ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 
-if(!(Test-Path '.\source')){
-  New-Item '.\source' -ItemType Directory
-}
+try {
+  if($clean) {
+    "msvc,sourceディレクトリを削除する"
+    Remove-Item ".\msvc" -Force -Recurse -Confirm:$False
+    Remove-Item ".\source" -Force -Recurse -Confirm:$False
+  }
 
-# FFmpegのレポジトリを取得する
-Set-Location ".\source"
+  "作業ディレクトリの作成"
+  if (!(Test-Path '.\msvc')) {
+    New-Item '.\msvc' -ItemType Directory
+  }
+
+  if (!(Test-Path '.\msvc\include')) {
+    New-Item '.\msvc\include' -ItemType Directory
+  }
+
+  if (!(Test-Path '.\msvc\include\gl')) {
+    New-Item '.\msvc\include\gl' -ItemType Directory
+  }
+
+  if (!(Test-Path '.\msvc\include\KHR')) {
+    New-Item '.\msvc\include\KHR' -ItemType Directory
+  }
+
+
+  if (!(Test-Path '.\source')) {
+    New-Item '.\source' -ItemType Directory
+  }
+
+  "FFmpegのレポジトリを取得する"
+  Set-Location ".\source"
  
-if(!(Test-Path ".\FFmpeg")){
-  Start-Process  "git"  -ArgumentList "clone https://github.com/ShiftMediaProject/FFmpeg.git" -Wait
-} else {
-  Set-Location ".\FFmpeg"
-  Start-Process  "git"  -ArgumentList "pull" -Wait
-  Set-Location '..\'
-}
+  if (!(Test-Path ".\FFmpeg")) {
+    Start-Process  "git"  -ArgumentList "clone https://github.com/ShiftMediaProject/FFmpeg.git" -Wait -NoNewWindow
+  }
+  else {
+    Set-Location ".\FFmpeg"
+    Start-Process  "git"  -ArgumentList "pull" -Wait -NoNewWindow
+    Set-Location '..\'
+  }
 
-# 依存ライブラリを取得する
-Set-Location ".\FFmpeg\SMP"
-Start-Process "cmd" -ArgumentList "/c project_get_dependencies.bat" -Wait
-Set-Location "..\..\"
+  "依存ライブラリを取得する"
+  Set-Location ".\FFmpeg\SMP"
+  Start-Process "cmd" -ArgumentList "/c project_get_dependencies.bat < ..\..\..\y.txt" -Wait -NoNewWindow
+  Set-Location "..\..\..\"
 
-# openglヘッダファイルを取得する
-Set-Location ".\msvc\include\gl"
-Invoke-WebRequest "https://www.khronos.org/registry/OpenGL/api/GL/glext.h" -OutFile '.\glext.h' 
-Invoke-WebRequest "https://www.khronos.org/registry/OpenGL/api/GL/wglext.h" -OutFile '.\wglext.h'
-Set-Location "..\KHR"
-Invoke-WebRequest "https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h" -OutFile '.\khrplatform.h'
-Set-Location "..\..\..\"
+  "openglヘッダファイルを取得する"
+  Set-Location ".\msvc\include\gl"
+  Invoke-WebRequest "https://www.khronos.org/registry/OpenGL/api/GL/glext.h" -OutFile '.\glext.h' -ErrorAction Stop
+  Invoke-WebRequest "https://www.khronos.org/registry/OpenGL/api/GL/wglext.h" -OutFile '.\wglext.h' -ErrorAction Stop
+  Set-Location "..\KHR"
+  Invoke-WebRequest "https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h" -OutFile '.\khrplatform.h' -ErrorAction Stop
+  Set-Location "..\..\..\"
 
-# nvcodecのヘッダファイルをコピーする
-Set-Location ".\source"
-if(!(Test-Path(".\nv-codec-headers"))){
-  Start-Process "git" -ArgumentList "clone https://github.com/FFmpeg/nv-codec-headers" -Wait
-} else {
-  Set-Location '.\nv-codec-headers'
-  Start-Process "git" -ArgumentList "pull"
-  Set-Location '..\'
-}
-Copy-Item -Path ".\source\nv-codec-headers\include" ".\msvc\include" -Force -Recurse
+  Set-Location ".\source"
 
-Set-Location ".\source"
+  "nvcodecのヘッダファイルを取得する"
+  if (!(Test-Path(".\nv-codec-headers"))) {
+    Start-Process "git" -ArgumentList "clone https://github.com/FFmpeg/nv-codec-headers" -Wait -NoNewWindow
+  }
+  else {
+    Set-Location '.\nv-codec-headers'
+    Start-Process "git" -ArgumentList "pull" -NoNewWindow
+    Set-Location '..\'
+  }
+  Copy-Item -Path ".\nv-codec-headers\include\ffnvcodec" "..\msvc\include" -Force -Recurse
 
-# AMFのヘッダファイルをコピーする
-if(!(Test-Path(".\AMF"))){
-  Start-Process "git" -ArgumentList "clone https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git" -Wait
-} else {
-  Set-Location '.\AMF'
-  Start-Process "git" -ArgumentList "pull" -Wait
-  Set-Location '..\'
-}
-Copy-Item -Path ".\source\AMF\amf\public\include" ".\msvc\include\AMF" -Force -Recurse
+  "AMFのヘッダファイルを取得する"
+  if (!(Test-Path(".\AMF"))) {
+    Start-Process "git" -ArgumentList "clone https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git" -NoNewWindow -Wait
+  }
+  else {
+    Set-Location '.\AMF'
+    Start-Process "git" -ArgumentList "pull" -Wait -NoNewWindow
+    Set-Location '..\'
+  }
+  Copy-Item -Path ".\AMF\amf\public\include" "..\msvc\include\AMF" -Force -Recurse
 
-# NASM を取得する
-if(!(Test-Path(".\VSNASM"))){
-  Start-Process "git" -ArgumentList "clone https://github.com/ShiftMediaProject/VSNASM.git"
-} else {
+  "NASM を取得する"
+  if (!(Test-Path(".\VSNASM"))) {
+    Start-Process "git" -ArgumentList "clone https://github.com/ShiftMediaProject/VSNASM.git" -NoNewWindow -Wait
+  }
+  else {
+    Set-Location ".\VSNASM"
+    Start-Process  "git"  -ArgumentList "pull" -Wait -NoNewWindow
+    Set-Location '..\'
+  }
+
+  "NASMのインストール"
   Set-Location ".\VSNASM"
-  Start-Process  "git"  -ArgumentList "pull" -Wait
+  Start-Process "cmd" -ArgumentList "/c install_script.bat < ..\..\y.txt" -Wait -Verb runAs
   Set-Location '..\'
-}
 
-# NASMのインストール
-Set-Location ".\VSNASM"
-Start-Process "cmd" -ArgumentList "/c install_script.bat" -Wait
-Set-Location '..\'
+  "YASM を取得する"
+  if (!(Test-Path(".\VSYASM"))) {
+    Start-Process "git" -ArgumentList "clone https://github.com/ShiftMediaProject/VSYASM.git" -NoNewWindow -Wait
+  }
+  else {
+    Set-Location ".\VSYASM"
+    Start-Process  "git"  -ArgumentList "pull" -Wait -NoNewWindow
+    Set-Location '..\'
+  }
 
-# YASM を取得する
-if(!(Test-Path(".\VSYASM"))){
-  Start-Process "git" -ArgumentList "clone https://github.com/ShiftMediaProject/VSYASM.git"
-} else {
+  "YASMのインストール"
   Set-Location ".\VSYASM"
-  Start-Process  "git"  -ArgumentList "pull" -Wait
+  Start-Process "cmd" -ArgumentList "/c install_script.bat < ..\..\y.txt" -Wait -Verb runAs
   Set-Location '..\'
+
+  "ffmpegをビルドする"
+  Set-Location ".\FFmpeg\SMP"
+  Start-Process "msbuild" -ArgumentList "ffmpeg_deps.sln /t:$($BuildType) /p:Configuration=`"$($ObjType)`";Platform=`"$($CpuType)`"" -NoNewWindow -Wait
+  Set-Location "..\..\..\"
+
+  "## 終了 ##"
+  
 }
-
-# YASMのインストール 
-Set-Location ".\VSYASM"
-Start-Process "cmd" -ArgumentList "/c install_script.bat" -Wait
-Set-Location '..\'
-
-Set-Location "..\" 
-
-# ビルドする
-
-Set-Location ".\source\FFmpeg\SMP"
-Start-Process "msbuild" -ArgumentList "ffmpeg_deps.sln /t:build /p:Configuration=Release;Platform='x64'"
-Set-Location "..\..\..\"
+catch {
+  "## エラー発生 ##"
+  $_
+}
+finally {
+  Set-Location $ScriptDir
+}
